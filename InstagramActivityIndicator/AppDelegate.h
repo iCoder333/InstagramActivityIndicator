f0495aedcfa4b703076417c2c99e9b922a4eadcf
iCoder333 <jansen0209@gmail.com>
2017-04-08 10:12:44 +0200

    ActivityIndicator
    Activity Indicator similar to Instagram's

---
+++ InstagramActivityIndicator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramActivityIndicator
+//
+//  Created by iCoder on 03/04/17.
+//  Copyright © 2017 iCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
